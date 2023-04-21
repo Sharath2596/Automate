7a58b8b6d5b0a8450ff545e81d63f63e24f5e42e
Sharath P <78840671+Sharath2596@users.noreply.github.com>
2023-04-21 15:00:57 +0530

    Add files via upload

---
+++ Different Workbook.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+
+import pandas as pd  # pip install pandas
+
+# Define & create output directory
+output_dir = Path(__file__).parent / "output"
+output_dir.mkdir(parents=True, exist_ok=True)
+
+# Define Excel file path
+excel_file = Path(__file__).parent / "Mysuru.xlsx"
+
+df = pd.read_excel(excel_file)
+column_name = "T_name"
+replace_symbols = ['>', '<', ':', '"', '/', '\\\\', '\|', '\?', '\*']
+df[column_name] = (
+    df[column_name].replace(replace_symbols, '', regex=True).str.strip().str.title()
+)
+unique_values = df[column_name].unique()
+
+for unique_value in unique_values:
+    df_output = df[df[column_name].str.fullmatch(unique_value)]
+    output_path = output_dir / f"{unique_value}.xlsx"
+    df_output.to_excel(output_path, sheet_name=unique_value[:31], index=False)
